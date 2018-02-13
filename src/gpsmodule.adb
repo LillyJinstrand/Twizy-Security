2c74c81d007e69b4ed2af113620c6903c51dca4f
Johan Erlandsson <erlandd6@gmail.com>
2018-02-13 15:10:48 +0100

    Test version of gps-module

---
+++ src/gpsmodule.adb
@@ -0,0 +1,35 @@
+package body gpsmodule with SPARK_Mode is
+
+   chalmers : constant box := (bottom_right_lon => 11.981237232685089,
+                bottom_right_lat => 57.68699206426933,
+                top_left_lat => 57.688440132300684,
+                top_left_lon => 11.97746068239212);
+   
+   
+   
+   function gpstest(X : in Lat; Y : in Lon) return Boolean
+     
+   is
+        
+      
+         
+   begin      
+      
+      if (X <= chalmers.top_left_lat and then 
+          chalmers.bottom_right_lat <= X and then
+          Y >= chalmers.top_left_lon and then
+          chalmers.bottom_right_lon >= Y) then
+
+         return True;
+      
+         
+      else
+         
+         return False;
+      
+      end if;
+      
+      
+
+   end gpstest;
+   end;
---
+++ src/gpsmodule.ads
@@ -0,0 +1,23 @@
+pragma SPARK_Mode(ON);
+
+package gpsModule with SPARK_Mode is
+
+   type Lon is delta 10.0**(-15) range -180.0 .. 180.0;
+   type Lat is delta 10.0**(-15) range -90.0 .. 90.0;
+   
+   type box is 
+      record
+         top_left_lat : Lat;
+         bottom_right_lat : Lat;
+         bottom_right_lon : Lon;
+         top_left_lon : Lon;
+      end record;
+   
+   function gpstest(X : in Lat; Y : in Lon) return Boolean with
+    Post => (if gpstest'Result then X >= 57.68699206426933 and X <= 57.688440132300684 and Y >= 11.97746068239212 and Y <= 11.981237232685089) and
+    (if not gpstest'Result then X < 57.68699206426933 or X > 57.688440132300684 or Y < 11.97746068239212 or Y > 11.981237232685089),
+     Global => null;
+
+
+
+end gpsModule;
