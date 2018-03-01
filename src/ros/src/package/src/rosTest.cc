477e39001785764673f709ef30a4321680640704
Hashem Hashem <hashemmm96@gmail.com>
2018-03-01 12:36:51 +0100

    Slight modifications to rosTest

--- src/ros/src/package/src/rosTest.cc
+++ src/ros/src/package/src/rosTest.cc
@@ -3,6 +3,29 @@
 #include <sstream>
 //#include "security.h"
 
+void canbusCallback(const std_msgs::String::ConstPtr& msg)
+{
+  ROS_INFO("I heard from canbus: [%s]", msg->data.c_str());
+  /* Filter out speed from canbus data
+  update_speed(); 
+  */
+}
+
+void controlCallback(const std_msgs::String::ConstPtr& msg)
+{
+  ROS_INFO("I heard from control: [%s]", msg->data.c_str());
+  /* This isn't even correct just a suggestion, we should probably 
+  if is_safe()
+	  vidarebefordra
+  */
+}
+
+void localizationCallback(const std_msgs::String::ConstPtr& msg)
+{
+  ROS_INFO("I heard from localization: [%s]", msg->data.c_str());
+  /* update_gps(); */
+}
+
 void perceptionCallback(const std_msgs::String::ConstPtr& msg)
 {
   ROS_INFO("I heard from perception: [%s]", msg->data.c_str());
@@ -15,41 +38,18 @@ void predictionCallback(const std_msgs::String::ConstPtr& msg)
   /* update_prediction(); Maybe combine prediction and perception? */
 }
 
-void controlCallback(const std_msgs::String::ConstPtr& msg)
-{
-  ROS_INFO("I heard from control: [%s]", msg->data.c_str());
-  /* This isn't even correct just a suggestion, we should probably 
-  if is_safe()
-	  vidarebefordra
-  */
-}
-
-void canbusCallback(const std_msgs::String::ConstPtr& msg)
-{
-  ROS_INFO("I heard from canbus: [%s]", msg->data.c_str());
-  /* Filter out speed from canbus data
-  update_speed(); 
-  */
-}
-
-void localizationCallback(const std_msgs::String::ConstPtr& msg)
-{
-  ROS_INFO("I heard from localization: [%s]", msg->data.c_str());
-  /* update_gps(); */
-}
-
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "rosTest");   
   ros::NodeHandle n;   
 
-  ros::Subscriber perception = n.subscribe("perception", 1000, perceptionCallback);
-  ros::Subscriber prediction = n.subscribe("prediction", 1000, predictionCallback);
-  ros::Subscriber control = n.subscribe("control", 1000, controlCallback);
-  ros::Subscriber canbus = n.subscribe("canbus", 1000, canbusCallback);
-  ros::Subscriber localization = n.subscribe("localization", 1000, localizationCallback);
+  ros::Subscriber canbus = n.subscribe("/apollo/canbus", 1000, canbusCallback);
+  ros::Subscriber control = n.subscribe("/apollo/control", 1000, controlCallback);
+  ros::Subscriber localization = n.subscribe("/apollo/localization", 1000, localizationCallback);
+  ros::Subscriber perception = n.subscribe("/apollo/perception", 1000, perceptionCallback);
+  ros::Subscriber prediction = n.subscribe("/apollo/prediction", 1000, predictionCallback);
   
-  ros::Publisher canbus_pub = n.advertise<std_msgs::String>("canbus", 1000);
+  ros::Publisher canbus_pub = n.advertise<std_msgs::String>("/apollo/canbus", 1000);
 
   ros::Rate loop_rate(1000);   
 
@@ -62,7 +62,7 @@ int main(int argc, char **argv)
     ss << "hello world " << count;
     msg.data = ss.str();
 
-    ROS_INFO("%s", msg.data.c_str());
+    //ROS_INFO("%s", msg.data.c_str());
 
     //canbus_pub.publish(msg);
 
