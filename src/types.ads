d819e515d152f44973cd0e59c9c76b39c2009668
Hashem Hashem <hashemmm96@gmail.com>
2018-03-08 13:05:42 +0100

    Renamed FixedNumber type to FloatingNumber to avoid confusion.

--- src/types.ads
+++ src/types.ads
@@ -1,7 +1,7 @@
 package types is
 
    -- 9 digits let's us represent 10^7m with cm accuracy
-   type FixedNumber is digits 9;
-   subtype Speed is FixedNumber range -80.0 .. 80.0;
+   type FloatingNumber is digits 9;
+   subtype Speed is FloatingNumber range -80.0 .. 80.0;
 
 end types;
