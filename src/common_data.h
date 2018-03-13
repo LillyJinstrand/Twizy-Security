cfa63d5d72ef021a4ce6fef6ef5a536080201f56
Simon Mölzer <evilhowser@gmail.com>
2018-03-13 11:52:49 +0100

    Add common data header

---
+++ src/common_data.h
@@ -0,0 +1,47 @@
+#ifndef COMMON_DATA_H
+#define COMMON_DATA_H
+
+struct path_point_ada {
+	double x;
+	bool valid_x;
+
+	double y;
+	bool valid_y;
+
+	double z;
+	bool valid_y;
+
+	// direction in x-y plane
+	double theta;
+	bool valid_theta;
+
+	// curvature in x-y plane
+	double kappa;
+	bool valid_kappa;
+
+	double dkappa; // first derivative
+	bool valid_dkappa;
+
+	double ddkappa; // second derivative
+	bool valid_ddkappa;
+
+	// accumulated dist from beginning of path
+	double s;
+	bool valid_s;
+}
+
+struct trajectory_point_ada {
+	path_point_ada path_point;
+	bool valid_point;
+
+	double v; // velocity
+	bool valid_v;
+
+	double a; // acceleration
+	bool valid_a;
+
+	double relative_time; // time from beginning of trajectory
+	bool valid_relative_time;
+}
+
+#endif // COMMON_DATA_H
--- src/prediction_data.h
+++ src/prediction_data.h
@@ -2,50 +2,8 @@
 #define PERCEPTION_DATA_H
 
 #include "perception_data.h"
+#include "common_data.h"
 
-struct path_point_ada {
-	double x;
-	bool valid_x;
-
-	double y;
-	bool valid_y;
-
-	double z;
-	bool valid_y;
-
-	// direction in x-y plane
-	double theta;
-	bool valid_theta;
-
-	// curvature in x-y plane
-	double kappa;
-	bool valid_kappa;
-
-	double dkappa; // first derivative
-	bool valid_dkappa;
-
-	double ddkappa; // second derivative
-	bool valid_ddkappa;
-
-	// accumulated dist from beginning of path
-	double s;
-	bool valid_s;
-}
-
-struct trajectory_point_ada {
-	path_point_ada path_point;
-	bool valid_point;
-
-	double v; // velocity
-	bool valid_v;
-
-	double a; // acceleration
-	bool valid_a;
-
-	double relative_time; // time from beginning of trajectory
-	bool valid_relative_time;
-
-}
 
 struct trajectory_ada {
 	double probability;
