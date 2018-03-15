216e7baa7d6d910a6931fe85be8a2760a705a6c4
joherlan <35494021+joherlan@users.noreply.github.com>
2018-03-15 16:51:59 +0100

    Start of the polygon gps
    Changed types from delta to digits in order to work
    Only used lat in order to make the types comparable

--- src/gpsmodule.ads
+++ src/gpsmodule.ads
@@ -2,8 +2,8 @@ pragma SPARK_Mode(ON);
 
 package gpsModule with SPARK_Mode is
 
-   type Lon is delta 10.0**(-15) range -180.0 .. 180.0;
-   type Lat is delta 10.0**(-15) range -90.0 .. 90.0;
+   type Lon is digits 8 range -180.0 .. 180.0;
+   type Lat is digits 8 range -90.0 .. 90.0;
    
    type box is 
       record
@@ -17,5 +17,7 @@ package gpsModule with SPARK_Mode is
     Post => (if gpstest'Result then X >= 57.68699206426933 and X <= 57.688440132300684 and Y >= 11.97746068239212 and Y <= 11.981237232685089) and
     (if not gpstest'Result then X < 57.68699206426933 or X > 57.688440132300684 or Y < 11.97746068239212 or Y > 11.981237232685089),
      Global => null;
+
+
 
 end gpsModule;
