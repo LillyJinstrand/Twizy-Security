4494cd32519c20d527f0640a533ebdff5faf1a2c
Emil Kurinkitos Jinstrand <emil.jinstrand@gmail.com>
2018-03-15 13:50:21 +0100

    Extracted all perception conversion function into perception_converter

---
+++ src/ros/src/package/include/perception_converter.h
@@ -0,0 +1,9 @@
+#ifndef PERCEPTION_CONVERTER_H
+#define PERCEPTION_CONVERTER_H
+
+#include "modules/perception/proto/perception_obstacle.pb.h"
+#include "perception_data.h"
+
+perception_obstacle_ada convert_obstacle(const apollo::perception::PerceptionObstacle&);
+
+#endif
---
+++ src/ros/src/package/src/perception_converter.cc
@@ -0,0 +1,54 @@
+#include "perception_converter.h"
+
+perception_obstacle_ada convert_obstacle(const apollo::perception::PerceptionObstacle& obs){
+    perception_obstacle_ada ada_obs = {};
+
+    if((ada_obs.valid_id = obs.has_id())){
+        ada_obs.id = obs.id();
+    }
+    if((ada_obs.valid_id = obs.has_velocity())){
+        if((ada_obs.velocity.valid_x = obs.velocity().has_x()))
+            ada_obs.velocity.x = obs.velocity().x();
+        if((ada_obs.velocity.valid_y = obs.velocity().has_y()))
+            ada_obs.velocity.y = obs.velocity().y();
+        if((ada_obs.velocity.valid_z = obs.velocity().has_z()))
+            ada_obs.velocity.z = obs.velocity().z();
+    }
+
+    if((ada_obs.valid_length = obs.has_length()))
+        ada_obs.length = obs.length();
+    if((ada_obs.valid_length = obs.has_width()))
+        ada_obs.width = obs.width();
+    if((ada_obs.valid_height = obs.has_height()))
+        ada_obs.height = obs.height();
+
+    if((ada_obs.valid_tracking_time = obs.has_tracking_time()))
+        ada_obs.tracking_time = obs.tracking_time();
+
+    if((ada_obs.valid_type = obs.has_type())){
+        switch(obs.type()){
+           case 0:
+               ada_obs.type = UNKNOWN;
+               break;
+           case 1:
+               ada_obs.type = UNKNOWN_MOVABLE;
+               break;
+           case 2:
+               ada_obs.type = UNKNOWN_UNMOVABLE;
+               break;
+           case 3:
+               ada_obs.type = PEDESTRIAN;
+               break;
+           case 4:
+               ada_obs.type = BICYCLE;
+               break;
+           case 5:
+               ada_obs.type = VEHICLE;
+               break;
+        }
+    }
+    if((ada_obs.valid_timestamp = obs.has_timestamp()))
+        ada_obs.timestamp = obs.timestamp();
+    if((ada_obs.valid_tracking_time = obs.has_tracking_time()))
+        ada_obs.tracking_time = obs.tracking_time();
+}
