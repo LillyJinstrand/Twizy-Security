e817ab631722830704230519a397340799c4bd47
Simon Mölzer <evilhowser@gmail.com>
2018-05-10 13:39:21 +0200

    Fix compile errors in converters

--- src/converters.ads
+++ src/converters.ads
@@ -5,10 +5,10 @@ with speed_data_h; use speed_data_h;
 with types;
 
 package converters with SPARK_Mode is
-
+   use Types;
    function double_to_speed (d : double) return Types.speed with
-       Pre => d > -80.0 and d < 80.0 ;
-       Post => double_to_speed'Result = Speed(d);
+       Pre => d > Double(-80.0) and d < Double(80.0),
+       Post => double_to_speed'Result = Types.Speed(d);
 
    function point_to_cart_x (p : point_3d) return Types.Cartesian_Coordinate with
        pre => p.x < 180.0 and -180.0 < p.x ;
@@ -20,7 +20,7 @@ package converters with SPARK_Mode is
        pre => p.z < 180.0 and -180.0 < p.z ;
 
    function speed_ada_to_speed (s : speed_ada) return Types.Speed with
-       pre => s.speed > -80.0 and s.speed < 80.0,
-       Post => Speed(s.speed) = speed_ada_to_speed'Result;
+       pre => s.speed > Double(-80.0) and s.speed < Double(80.0),
+       Post => Types.Speed(s.speed) = speed_ada_to_speed'Result;
 
 end converters;
